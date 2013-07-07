cd1a82ff18a7696fa7aa874a5931952494a6e252
wbvg-mac <will@mylittleshowpony.com>
2013-07-07 21:21:37 +1000

    first commit

--- stocks.rb
+++ stocks.rb
@@ -1,9 +1,30 @@
 require 'yahoofinance'
-quote_type = YahooFinance::RealTimeQuote
-quote_symbols = "yhoo,goog,appl,ko,ebay,jnj,intc,mcd,nke,nwsa"
 
+class Stock
+  attr_accessor :symbol, :qty
 
-YahooFinance::get_quotes( quote_type, quote_symbols ) do |qt|
-    puts "QUOTING: #{qt.symbol}"
-    puts qt.to_s
+  def initialize(symbol, qty)
+    @symbol = symbol
+    @qty = qty
+  end
+
+  def buy(qty)
+    @qty += qty
+  end
+
+  def sell(shares)
+    @qty -= qty if qty <= @qty
+  end
+
+  def quote
+     Stock.quote(@symbol) * @qty
+  end
+
+  def to_s
+    "Purchased #{@qty} shares of #{@symbol} worth $#{quote.round}"
+  end
+
+  def Stock.quote(symbol)
+    YahooFinance::get_quotes(YahooFinance::StandardQuote, symbol)[symbol].lastTrade
+  end
 end
